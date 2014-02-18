7ff9863d6dc8722140cefdddc7291c4d282f33a0
Simon Mathieu <simon.math@gmail.com>
2014-02-18 11:10:25 -0800

    Order jobs by id

--- app/controllers/queue_classic_admin/application_controller.rb
+++ app/controllers/queue_classic_admin/application_controller.rb
@@ -3,7 +3,7 @@ module QueueClassicAdmin
     protected
     def filter_jobs(klass)
       @klass = klass
-      @queue_classic_jobs = klass.scoped
+      @queue_classic_jobs = klass.order("id DESC")
       if params[:q_name].present?
         @queue_classic_jobs = @queue_classic_jobs.where(q_name: params[:q_name])
       end
