99d0b273c16a7eb3e221a10b5e59a75f70fcc39f
Chris Hanks <christopher.m.hanks@gmail.com>
2014-04-03 15:46:19 -0700

    Get pagination working.

--- lib/queue_classic_admin.rb
+++ lib/queue_classic_admin.rb
@@ -6,10 +6,12 @@ module QueueClassic
     get '/' do
       table_name = QC::TABLE_NAME
       @column_names = get_column_names(table_name)
+      offset = (page_number - 1) * jobs_per_page
+
       if params[:q_name]
-        @queue_classic_jobs = execute("SELECT * FROM #{table_name} WHERE q_name = $1", [params[:q_name]])
+        @queue_classic_jobs = execute("SELECT * FROM queue_classic_jobs WHERE q_name = $1 ORDER BY id DESC LIMIT $2 OFFSET $3", [params[:q_name], jobs_per_page, offset])
       else
-        @queue_classic_jobs = execute("SELECT * FROM #{table_name}")
+        @queue_classic_jobs = execute("SELECT * FROM queue_classic_jobs ORDER BY id DESC LIMIT $1 OFFSET $2", [jobs_per_page, offset])
       end
 
       @queue_counts = execute("SELECT q_name, count(*) FROM #{table_name} GROUP BY q_name")
@@ -51,8 +53,24 @@ module QueueClassic
         end
       end
 
+      def jobs_per_page
+        (ENV["JOBS_PER_PAGE"] || 50).to_i
+      end
+
       def execute(*args)
         QC.default_conn_adapter.connection.exec(*args).map{|h| OpenStruct.new(h)}
+      end
+
+      def page_number
+        (params[:page] || 1).to_i
+      end
+
+      def previous_page?
+        page_number > 1
+      end
+
+      def next_page?
+        @queue_classic_jobs.count == jobs_per_page
       end
     end
   end
