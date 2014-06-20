5d42687525fa657f802dab1bdc42e91f35961c91
Paul Sadauskas <psadauskas@gmail.com>
2014-06-20 12:39:05 -0700

    Extract custom search fields into JobCommon
    Also adds instructions to README

--- app/models/queue_classic_admin/queue_classic_job.rb
+++ app/models/queue_classic_admin/queue_classic_job.rb
@@ -2,19 +2,5 @@ module QueueClassicAdmin
   class QueueClassicJob < ActiveRecord::Base
     include JobCommon
     self.table_name = 'queue_classic_jobs'
-
-    SEARCHABLE_COLUMNS = [
-      :method,
-      :args
-    ]
-
-    def self.search(query)
-      sql = SEARCHABLE_COLUMNS.inject([]) do |sql, field|
-        sql << "#{field} LIKE ?"
-      end.join(" OR ")
-
-      wildcard_query = ["%", query, "%"].join
-      relation.where(sql, *([wildcard_query] * SEARCHABLE_COLUMNS.size))
-    end
   end
 end
