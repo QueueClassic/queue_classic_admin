69a288e1f78c4a47ead08aa2f37b9877bd798a65
Simon Mathieu <simon.math@gmail.com>
2014-07-11 00:15:37 -0700

    Don't die while migrating if queue_classic_later is not installed

--- db/migrate/20130626182618_add_id_to_later.rb
+++ db/migrate/20130626182618_add_id_to_later.rb
@@ -1,5 +1,7 @@
 class AddIdToLater < ActiveRecord::Migration
   def change
-    add_column :queue_classic_later_jobs, :id, :primary_key
+    if ActiveRecord::Base.connection.table_exists?(:queue_classic_later_jobs)
+      add_column :queue_classic_later_jobs, :id, :primary_key
+    end
   end
 end
--- db/migrate/20130627175128_add_created_column.rb
+++ db/migrate/20130627175128_add_created_column.rb
@@ -1,7 +1,9 @@
 class AddCreatedColumn < ActiveRecord::Migration
   def up
     %w(queue_classic_later_jobs queue_classic_jobs).each do |table|
-      execute "ALTER TABLE #{table} ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT now();"
+      if ActiveRecord::Base.connection.table_exists?(:table)
+        execute "ALTER TABLE #{table} ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT now();"
+      end
     end
   end
 
