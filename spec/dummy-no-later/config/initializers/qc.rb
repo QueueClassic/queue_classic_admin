e8af7768a8fcd3dcffee62d114732b125a5bc5f3
Jean-Philippe Boily <j@jipi.ca>
2014-08-26 10:26:49 -0400

    fix dummy-not-later app

--- spec/dummy-no-later/config/initializers/qc.rb
+++ spec/dummy-no-later/config/initializers/qc.rb
@@ -1,3 +1,2 @@
 require 'queue_classic'
-QC::Conn.connection = ActiveRecord::Base.connection.raw_connection
-
+QC.default_conn_adapter = QC::ConnAdapter.new(ActiveRecord::Base.connection.raw_connection)
--- spec/dummy/config/initializers/qc.rb
+++ spec/dummy/config/initializers/qc.rb
@@ -1,3 +1,2 @@
 require 'queue_classic'
 QC.default_conn_adapter = QC::ConnAdapter.new(ActiveRecord::Base.connection.raw_connection)
-
