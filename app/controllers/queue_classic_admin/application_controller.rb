d2dda4a99396641b281b80da26193483d4a3d308
Jean-Philippe Boily <j@jipi.ca>
2015-02-17 16:48:51 -0500

    update the job counts in the UI to reflect state

--- app/controllers/queue_classic_admin/application_controller.rb
+++ app/controllers/queue_classic_admin/application_controller.rb
@@ -3,6 +3,12 @@ module QueueClassicAdmin
     protected
     def filter_jobs(klass)
       @klass = klass
+      if self.class == QueueClassicScheduledJobsController
+        @scoped_jobs = klass.scheduled
+      else
+        @scoped_jobs = klass.ready
+      end
+
       @queue_classic_jobs = klass.order("id DESC")
       if params[:q_name].present?
         @queue_classic_jobs = @queue_classic_jobs.where(q_name: params[:q_name])
