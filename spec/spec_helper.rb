6eeba1837c54c9abec6513a09fbd5de11379d404
Jean-Philippe Boily <j@jipi.ca>
2015-02-18 14:09:21 -0500

    add metadata to specs for RSpec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.render_views
-  
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -38,4 +38,12 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # RSpec versions before 3.0.0 automatically added metadata to specs based on
+  # their location on the filesystem. This was both confusing to new users and not
+  # desirable for some veteran users.
+  #
+  # In RSpec 3, this behavior must be explicitly enabled:
+  # https://relishapp.com/rspec/rspec-rails/docs/directory-structure
+  config.infer_spec_type_from_file_location!
 end
